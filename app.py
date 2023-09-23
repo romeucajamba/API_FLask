1aefeb624e5e777df703b14080dea17e3ae5fc39
RomeuCajamba <117159526+romeucajamba@users.noreply.github.com>
2023-09-23 15:01:19 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,62 @@
 from flask import Flask
 from flask import jsonify
-from flask import json
+from markupsafe import escape
 from flask import request
-# jsonify permite returnar os dados em formato JSON
+from flask import json
 
 app = Flask(__name__)
 
-@app.route('/')
+developer = [
+    {
+        'id': 0,
+        'nome': 'Tomás',
+        'habilidades':['python', 'flask'],
+    },
+    {
+        'id': 1,
+        'nome': 'Cajmaba',
+        'habilidades': ['python', 'django']
+    },
+]
 
-def firs_aplication():
-    return jsonify({'id': 1, 'nome': 'Romeu', 'cargo': 'Developer', 'email': 'romeucajamba@gmail.com', 'empresa': 'ApeixomCode'})
+@app.route('/', methods=['GET','PUT', 'DELETE', 'POST'])
+
+def developers():
+    if request.method == 'GET':
+        try:
+            response = developer
+        except IndexError:
+            response = {'status': 'Erro', 'message': 'Desenvolvedor não existe!'}
+        except Exception:
+            message = 'Messagem de erro desconhecido, procure o ADM da API'
+            staus = 'Erro'
+            response = {'status': staus, 'message': message}
+        return jsonify(response)
+
+    elif request.method == 'PUT':
+        dados = json.loads(request.data)
+        soft = developer
+        return jsonify(dados)
+
+    elif request.method == 'DELETE':
+        developer.pop()
+        return jsonify({'status': 'Sucesso', 'message': 'Registro excluido'})
+
+# Inserir dados e mostrar lista de todos
+@app.route('/', methods=['POT', 'GET'])
+def lista():
+    if request.method == 'POST':
+        dados = json.loads(request.data)
+        position = len(developer)
+        dados[id] = position
+        developer.appednd(dados)
+        return jsonify(developer)
+
+    elif request.methods == 'GET':
+        return jsonify(developer)
+
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
